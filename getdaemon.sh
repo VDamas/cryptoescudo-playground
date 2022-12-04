e2614c36dea167324e396be11416acc9847f5fae
VDamas <vitoriodamas@gmail.com>
2022-12-04 15:30:06 +0000

    Update getdaemon.sh

--- getdaemon.sh
+++ getdaemon.sh
@@ -8,7 +8,6 @@ else
     cd tmp
     wget http://cryptoescudo.pt/download/01030000/linux/cryptoescudo-1.3.0.0-linux.zip
     unzip -o cryptoescudo-1.3.0.0-linux.zip -d ./cryptoescudo
-    cd ..
-    cp -R cryptoescudo/cryptoescudo-1.3.0.0-linux/64/* ./cryptoescudo
+    cp -R cryptoescudo/cryptoescudo-1.3.0.0-linux/64/* ../cryptoescudo
     chmod +x $DAEMON
 fi
