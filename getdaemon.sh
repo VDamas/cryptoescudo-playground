464dcf57fd2ab6e0bede43acd9f38e6d9904d427
VDamas <vitoriodamas@gmail.com>
2022-12-04 15:36:25 +0000

    Update getdaemon.sh

--- getdaemon.sh
+++ getdaemon.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DAEMON=/workspace/cryptoescudo-playground/cryptoescudo/cryptoescudod
+DAEMONCONF=/workspace/cryptoescudo-playground/cryptoescudo/data/cryptoescudo.conf
 if [ -f "$DAEMON" ]; then
     echo "$DAEMON exists."
 else
@@ -10,4 +11,16 @@ else
     unzip -o cryptoescudo-1.3.0.0-linux.zip -d ./cryptoescudo
     cp -R cryptoescudo/cryptoescudo-1.3.0.0-linux/64/* ../cryptoescudo
     chmod +x $DAEMON
+    
+    # Create cryptoescudo.conf
+    rpcpass=$(openssl rand -hex 32) # generate pass
+    sudo tee "$DAEMONCONF" > /dev/null <<EOF
+    rpcuser=cryptoescudorpc
+    rpcpassword=$rpcpass
+    rpcport=61142
+    rpcallowip=127.0.0.1
+    server=1
+    listen=1
+    txindex=1
+    EOF
 fi
