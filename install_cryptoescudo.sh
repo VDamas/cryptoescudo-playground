aa91d3590af8a4662b38b43a992f6ae2f22b077f
vdamas <vitoriodamas@gmail.com>
2022-12-08 15:05:19 +0000

    Update install_cryptoescudo.sh

--- install_cryptoescudo.sh
+++ install_cryptoescudo.sh
@@ -60,15 +60,28 @@ sudo chmod +x $DAEMONQUERY
 sudo tee "$DAEMONCHAINUPD" > /dev/null <<'EOF'
 cd /workspace/cryptoescudo-playground/cryptoescudo
 
-# download from cryptoescudo.work (slower, but updated at 8am every day)
-#wget https://letsencrypt.org/certs/lets-encrypt-r3.pem
-#wget https://cryptoescudo.work/getchain --ca-certificate=lets-encrypt-r3.pem
-
-# download from google drive
-SHAREID=1tlrB2WCa4ijeUan-hRc-kaRyZbER1k8n
-CONFIRM="`wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies 'https://docs.google.com/uc?export=download&id=$SHAREID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p'`"
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$SHAREID" -O cryptoescudo.tar.gz 
-rm -rf /tmp/cookies.txt
+PS3="Choose chain download source: "
+options=('Cryptoescudo.work(slower / up-to-date)' 'Google Drive (faster / not up-to-date)')
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Cryptoescudo.work (slower / up-to-date)")
+            # download from cryptoescudo.work (slower, but updated at 8am every day)
+            wget https://letsencrypt.org/certs/lets-encrypt-r3.pem
+            wget https://cryptoescudo.work/getchain --ca-certificate=lets-encrypt-r3.pem
+			exit
+            ;;
+        "Google Drive (faster / maybe up-to-date)")
+            # download from google drive
+            SHAREID=1tlrB2WCa4ijeUan-hRc-kaRyZbER1k8n
+            CONFIRM="`wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies 'https://docs.google.com/uc?export=download&id=$SHAREID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p'`"
+            wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$SHAREID" -O cryptoescudo.tar.gz 
+            rm -rf /tmp/cookies.txt
+			exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
 ./kill_daemon.sh
 
